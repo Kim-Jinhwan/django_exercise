cdf20bf01838feb6de0eb66d3bb14705159139ff
Kim, Jinhwan <kimjh@cashwalk.io>
2022-01-12 23:47:14 +0900

    no message

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 from django.http import Http404
 from .models import Question
 
-
-# Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
